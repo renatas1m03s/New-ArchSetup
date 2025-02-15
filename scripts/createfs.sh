bdba77d404265a2d13ffe685738580a88904a3d9
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2025-02-15 19:02:14 -0300

    Add files via upload

--- scripts/createfs.sh
+++ scripts/createfs.sh
@@ -20,24 +20,24 @@ while true; do
     if [[ "$systemdisk" == "1" ]]; then
         echo -e "\n";
     else
-        echo -e 'Script will format EFI - /dev/'$systemdisk'1 - with FAT32 filesystem\n';
-        mkfs.fat -F32 -n EFI /dev/$systemdisk'1';
+        echo -e 'Script will format EFI - /dev/'$systemdisk'1 - with FAT32 filesystem\n'
+        mkfs.fat -F32 -n EFI /dev/$systemdisk'1'
 
-        echo -e '\nScript will format Boot - /dev/'$systemdisk'2 - with ext4 filesystem\n';
-        mkfs.ext4 -L Boot /dev/$systemdisk'2';
+        echo -e '\nScript will format Boot - /dev/'$systemdisk'2 - with ext4 filesystem\n'
+        mkfs.ext4 -L Boot /dev/$systemdisk'2'
 
         echo -e '\nScript will format Root - /dev/'$systemdisk'3 - with ext4 filesystem\n';
-        mkfs.ext4 -L Root /dev/$systemdisk'3';
+        mkfs.ext4 -L Root /dev/$systemdisk'3'
 
-        echo -e '\nMounting file system and enabling swapfile\n';
-        mount /dev/$systemdisk'3' /mnt;
-        mkdir /mnt/boot;
-        mount /dev/$systemdisk'2' /mnt/boot;
-        mkdir /mnt/boot/efi;
-        mount /dev/$systemdisk'1' /mnt/boot/efi;
-        mkdir /mnt/home;
-        mount /dev/$systemdisk'4' /mnt/home;
-        break;
+        echo -e '\nMounting file system and enabling swapfile\n'
+        mount /dev/$systemdisk'3' /mnt
+        mkdir /mnt/boot
+        mount /dev/$systemdisk'2' /mnt/boot
+        mkdir /mnt/boot/efi
+        mount /dev/$systemdisk'1' /mnt/boot/efi
+        mkdir /mnt/home
+        mount /dev/$systemdisk'4' /mnt/home
+        break
    fi
 done
 
@@ -50,19 +50,19 @@ while true; do
     read -p "\n\n#### Choose your option:" -n 1 response
 
     case "$response" in
-        [1]) export swapsize="8192M";
-        [2]) export swapsize="16384M";
+        [1]) export swapsize="8192M";;
+        [2]) export swapsize="16384M";;
         [3]) export swapsize="65535M";;
-        [4]) read -p "\n\nEnter swap file size: " swapsize;
+        [4]) read -p "\n\nEnter swap file size: " swapsize;;
           *) echo -e "\n"
     esac
 
     if [ $swapsize == "0"]; then
-        echo -e "\n";
+        echo -e "\n"
     else
-        mkswap -U clear --size $swapsize --file /mnt/swapfile;
-        swapon /mnt/swapfile;
-        break;;
+        mkswap -U clear --size $swapsize --file /mnt/swapfile
+        swapon /mnt/swapfile
+        break
     fi
 done
 
