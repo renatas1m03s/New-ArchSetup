0bdf37e2ed1af3794f140b15739ba440c43277a6
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2025-02-16 01:05:47 -0300

    Add files via upload

--- prepare.sh
+++ prepare.sh
@@ -14,8 +14,10 @@ pacman -Sy --noconfirm archlinux-keyring
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm git
-git clone https://github.com/renatas1m03s/New-ArchSetup ArchSetup
 
+mkdir /root/ArchSetup
+mount -t tmpfs -o size=1024m ArchSetup /root/ArchSetup
 
+git clone https://github.com/renatas1m03s/New-ArchSetup /root/ArchSetup
 
 echo -e "\n\n#### System prepared, now just execute \"sh /root/ArchSetup/first.sh\" script...\n"
