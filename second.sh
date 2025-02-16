91c017b2f0d12b9338c45f02f0562e7d80261ef2
renatas1m03s <renata.s1m03s@gmail.com>
2025-02-15 23:55:12 -0300

    Added via command-line

--- second.sh
+++ second.sh
@@ -3,24 +3,26 @@
 ## Bash script - basic archlinux install
 ## By Renata Maria - renata.s1m03s@gmail.com
 ## 
-## This install basic components to achive a minimal functional Arch Linux System
+## This install a Desktop Enviroment on Arch Linux system
 ##
 ## Version 0.1
 ##
 
+clear
+
 echo -e "\n\n########################################################\n\n    Installing a DE on ArchLinux System\n\n########################################################\n\n"
 
 export response="1"
-
 while true; do
 	read -p "#### Configure keyboard and timezone (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) sudo localectl set-keymap us-acentos;
 		      localectl;
+		      echo -e "\n\n";
 		      sudo timedatectl set-timezone America/Fortaleza;
 		      sudo timedatectl set-ntp true;
 		      timedatectl;
+		      echo -e "\n\n";
 		      break;;
 		[nN]) echo -e "\n"; break;;
 		*) echo -e "\n"
@@ -28,10 +30,8 @@ while true; do
 done
 
 export response="1"
-
 while true; do
 	read -p "#### Proceed with yay installation (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) sh /home/ArchSetup/scripts/yay.sh;
 		      break;;
@@ -41,12 +41,9 @@ while true; do
 done
 
 export response="1"
-
 while true; do
     echo -e "\n\n### Choose your graphic driver situation:\n     1 - Mesa only\n     2 - Nvidia Open plus Mesa\n     3 - Nvidia only\n     4 - Nothing\n\n"
-
 	read -p "#### Option: " -n 1 response
-
 	case "$response" in
 		[1]) sh /home/ArchSetup/scripts/mesa.sh;
 		      break;;
@@ -60,10 +57,8 @@ while true; do
 done
 
 export response="1"
-
 while true; do
 	read -p "#### Prepare system for Davinci Resolve with AMD GPU (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) yay -Sy --noconfirm opencl-amd;
 		      break;;
@@ -73,12 +68,9 @@ while true; do
 done
 
 export response="1"
-
 while true; do
     echo -e "\n\n### Choose your Desktop Enviroment:\n     1 - Gnome\n     2 - Plasma\n     3 - Nothing\n\n"
-
 	read -p "#### Option: " -n 1 response
-
 	case "$response" in
 		[1]) sh /home/ArchSetup/scripts/gnome.sh;
 		      break;;
@@ -90,10 +82,8 @@ while true; do
 done
 
 export response="1"
-
 while true; do
 	read -p "#### Configure eno1 with fixed ip address (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) fish /home/ArchSetup/scripts/network.sh;
 		      break;;
@@ -103,10 +93,8 @@ while true; do
 done
 
 export response="1"
-
 while true; do
 	read -p "#### Move renata.bak folders (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) mv -v /home/renata.bak/{AppImage,Documents,Downloads,Pictures,Scripts,SystemInfo,Temp,Videos}  ~/
 		      break;;
@@ -116,10 +104,19 @@ while true; do
 done
 
 export response="1"
+while true; do
+	read -p "#### Install MS-Fonts (y/n)? " -n 1 response
+	case "$response" in
+		[yY]) sh /home/ArchSetup/scripts/msfonts.sh;
+		      break;;
+		[nN]) echo -e "\n"; break;;
+		*) echo -e "\n"
+	esac
+done
 
+export response="1"
 while true; do
 	read -p "#### Install office and colaborative apps (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) sh /home/ArchSetup/scripts/apps.sh;
 		      break;;
@@ -129,10 +126,8 @@ while true; do
 done
 
 export response="1"
-
 while true; do
 	read -p "#### Install QEMU and Virt-Manager (y/n)? " -n 1 response
-	
 	case "$response" in
 		[yY]) sh /home/ArchSetup/scripts/virt-manager.sh;
 		      break;;
@@ -144,10 +139,8 @@ done
 echo -e "\n\n#### Setup finished \n\n"
 
 export response="1"
-
 while true; do
         read -p "### Reboot (y/n)? " -n 1 response
-
         case "$response" in
                 [yY]) sudo reboot; 
                       break;;
