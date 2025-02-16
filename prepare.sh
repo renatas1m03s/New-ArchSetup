f1b820ee2c499f427a956bb8260027628eee3165
Renata Simões <66223162+renatas1m03s@users.noreply.github.com>
2025-02-16 01:10:49 -0300

    Add files via upload

--- prepare.sh
+++ prepare.sh
@@ -10,13 +10,15 @@
 
 echo -e "\n\n########################################################\n\n    Preparing system for ArchLinux\n\n########################################################\n\n"
 
+mkdir -v /root/ArchSetup
+mount -t tmpfs -o size=1024m ArchSetup /root/ArchSetup
+mount | grep ArchSetup
+sleep 5
+
 pacman -Sy --noconfirm archlinux-keyring
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Sy --noconfirm git
-
-mkdir /root/ArchSetup
-mount -t tmpfs -o size=1024m ArchSetup /root/ArchSetup
 
 git clone https://github.com/renatas1m03s/New-ArchSetup /root/ArchSetup
 
