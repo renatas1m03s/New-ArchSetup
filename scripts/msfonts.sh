511b7ea063f6f7bab6e471c05f7cea6d901ecd09
renatas1m03s <renata.s1m03s@gmail.com>
2025-02-15 23:48:59 -0300

    Added via command-line

---
+++ scripts/msfonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "\n\n### Instaling MS-Fonts...\n"
+
+7z x /home/ArchSetup/assets/msfontz.7z.001 -o/tmp/
+sudo mkdir /usr/share/fonts/WindowsFonts
+sudo cp -rv /tmp/MS-Fonts/* /usr/share/fonts/WindowsFonts/
+sudo chmod 644 /usr/share/fonts/WindowsFonts/*
+sudo fc-cache –force
+
+echo "\n\n### MS-Fonts installed\n
